cd6076e002b7ead47f732f494f3c3f4ed7b827ae
Leandro Machado <fahurleandro@gmail.com>
2022-02-10 16:22:25 -0800

    fetch api exercises finished

--- src/app.js
+++ src/app.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.status(200).send("Sanity test...");
+  res.status(200).sendFile("index.html");
 });
 
 app.listen(3000, function () {
